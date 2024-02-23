74da4a1a695ed84d61f601e631fc080b6d5b17ec
Sak Khakna <sthissakkhakna@gmail.com>
2024-02-23 14:06:54 +0700

    edit nav and sidebar and add home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,22 @@
 function Home() {
-  return <section className="p-4 md:p-10">Home</section>;
+	return (
+		<div className="max-w-5xl mx-auto">
+			<section className="w-full h-72 bg-[#D9D9D9] p-4 md:p-10 mx-auto ">
+				Banner
+			</section>
+			<div className="w-full mx-auto">
+				<h1 className="text-2xl font-bold my-4">Report & Complaint System</h1>
+				<div className="grid grid-cols-3 grid-row-2 gap-4">
+					<div className="w-full h-52 bg-[#D9D9D9]">Report 1</div>
+					<div className="w-full h-52 bg-[#D9D9D9]">Report 2</div>
+					<div className="w-full h-52 bg-[#D9D9D9]">Report 3</div>
+					<div className="w-full h-52 bg-[#D9D9D9]">Report 4</div>
+					<div className="w-full h-52 bg-[#D9D9D9]">Report 5</div>
+					<div className="w-full h-52 bg-[#D9D9D9]">Add Report</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,27 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import BottomNavigation from "./BottomNavigation";
+import Logo from "../assets/Logo.png";
 
 function AppLayout() {
-  return (
-    <div className="flex flex-col xl:flex-row">
-      <aside className="fixed h-screen top-0 left-0 bottom-0 bg-green-600">
-        <div className="w-[230px] p-4 md:p-10 h-screen hidden xl:flex flex-col gap-10">
-          Aside
-        </div>
-      </aside>
-      <main className="w-full xl:ml-[230px]">
-        <Header />
-        <Outlet />
-      </main>
-      <BottomNavigation />
-    </div>
-  );
+	return (
+		<div className="flex flex-col xl:flex-row">
+			<aside className="w-1/6 fixed h-screen top-0 left-0 bottom-0 ">
+				<div className="w-[230px] p-4 md:p-10 h-screen hidden xl:flex flex-col gap-10">
+					<img src={Logo} alt="Logo" className="w-28" />
+					<dix className="flex flex-col text-lg font-bold">
+						<a>Home</a>
+						<a>Report</a>
+					</dix>
+				</div>
+			</aside>
+			<main className="w-full xl:ml-[230px]">
+				<Header />
+				<Outlet />
+			</main>
+			<BottomNavigation />
+		</div>
+	);
 }
 
 export default AppLayout;
